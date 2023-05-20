45d89c897f97eda729a8c2eebeb67485e487881c
Renato Freitas <natogfreitas@gmail.com>
2023-05-20 00:22:49 -0300

    feat: add base entity class

---
+++ ILikeThisFood.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILikeThisFood.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+    }
+}
