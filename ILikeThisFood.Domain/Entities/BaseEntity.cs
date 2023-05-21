64f0ccf67866af355912ca326498e22f90e6c0f3
Renato Freitas <natogfreitas@gmail.com>
2023-05-20 23:11:17 -0300

    fix: change properties of base entity from privates to protected

--- ILikeThisFood.Domain/Entities/BaseEntity.cs
+++ ILikeThisFood.Domain/Entities/BaseEntity.cs
@@ -8,8 +8,8 @@ namespace ILikeThisFood.Domain.Entities
 {
     public abstract class BaseEntity
     {
-        public Guid Id { get; private set; }
-        public DateTime CreatedAt { get; private set; }
-        public DateTime UpdatedAt { get; private set; }
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
     }
 }
