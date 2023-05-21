8da39693d9ae774c7b96189a877b196036afdf3e
Renato Freitas <natogfreitas@gmail.com>
2023-05-20 23:12:41 -0300

    feat: add CrossCutting project and IocBootstrapper

---
+++ ILikeThisFood.Infra.CrossCutting/IocBootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ILikeThisFood.Infra.CrossCutting
+{
+    public static class IocBootstrapper
+    {
+        public static void RegisterRepositories(this IServiceCollection services)
+        {
+        }
+
+        public static void RegisterServices(this IServiceCollection services)
+        {
+        }
+    }
+}
