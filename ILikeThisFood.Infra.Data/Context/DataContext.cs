9f256df1698a23c8b0d86ce95d0c48e6663cb90d
Renato Freitas <natogfreitas@gmail.com>
2023-05-20 23:14:04 -0300

    feat: add mongodb database configuration

---
+++ ILikeThisFood.Infra.Data/Context/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILikeThisFood.Infra.Data.Context
+{
+    public class DataContext
+    {
+        protected readonly IMongoDatabase _mongoDatabase;
+        protected readonly string _collectionName;
+
+        public DataContext(string collection)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            _mongoDatabase = client.GetDatabase("ILikeThisFood"); ;
+            _collectionName = collection;
+        }
+
+    }
+}
